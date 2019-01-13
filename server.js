3d84d6d3b1798ab787852e963a2f1ace7e8380a5
Daniel Richards <drichards211@yahoo.com>
2019-01-13 16:18:06 -0500

    Add runServer() and closeServer() to server.js

--- server.js
+++ server.js
@@ -64,6 +64,35 @@ app.put('/blog-posts/:id', jsonParser, (req, res) => {
   res.status(204).end()
 })
 
+let server
+
+function runServer() {
+  const port = process.env.PORT || 8080
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`)
+        resolve(server)
+      })
+      .on("error", err => {
+        reject(err)
+      })
+  })
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server")
+    server.close(err => {
+      if (err) {
+        reject(err)
+        // so we don't also call `resolve()`
+        return
+      }
+      resolve()
+    })
+  })
+}
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
