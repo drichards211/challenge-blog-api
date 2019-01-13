8afd30922b0e779e52dd290b42b6c8444bb1b66c
Daniel Richards <drichards211@yahoo.com>
2019-01-13 16:14:54 -0500

    Remove semicolons from server.js

--- server.js
+++ server.js
@@ -41,30 +41,30 @@ app.delete('/blog-posts/:id', (req, res) => {
 app.put('/blog-posts/:id', jsonParser, (req, res) => {
   const requiredFields = ['id', 'title', 'content', 'author', 'publishDate']
   for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
+    const field = requiredFields[i]
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
+      console.error(message)
+      return res.status(400).send(message)
     }
   }
   if (req.params.id !== req.body.id) {
-    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
-    console.error(message);
-    return res.status(400).send(message);
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`
+    console.error(message)
+    return res.status(400).send(message)
   }
-  console.log(`Updating blog post \`${req.params.id}\``);
+  console.log(`Updating blog post \`${req.params.id}\``)
   BlogPosts.update({
     id: req.params.id,
     title: req.body.title,
     content: req.body.content,
     author: req.body.author,
     publishDate: req.body.publishDate
-  });
-  res.status(204).end();
+  })
+  res.status(204).end()
 })
 
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
+})
