8e932b1364b72cbc3400dbb0e64150deaade926c
Daniel Richards <drichards211@yahoo.com>
2019-01-13 20:52:00 -0500

    Add get-test for blog-post

--- test/test.js
+++ test/test.js
@@ -1,3 +1,32 @@
 const expect = require('chai').expect
 const chaiHttp = require('chai-http')
 const { app, runServer, closeServer } = require('../server')
+
+chai.use(chaiHttp)
+
+describe("Blog Posts", function() {
+  before(function() {
+    return runServer()
+  });
+  after(function() {
+    return closeServer()
+  });
+  it("should list blog posts on GET", function() {
+    return chai
+      .request(app)
+      .get("/blog-posts")
+      .then(function(res) {
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body).to.be.a("array")
+
+        // because we create a blog post on app load
+        expect(res.body.length).to.be.at.least(1)
+        const expectedKeys = ["id", "title", "content", "author", "publishDate"]
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object")
+          expect(item).to.include.keys(expectedKeys)
+        })
+      })
+  })
+})
